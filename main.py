d4e17dd68835dc1d5a1668363c0c40fa6ecb1eb0
liloche <liloche68990@gmail.com>
2024-08-06 21:03:52 +0200

    class Piont et Plateau crées

---
+++ main.py
@@ -0,0 +1,17 @@
+class piont:
+    def __init__(self, famille, couleur, numero):
+        self.famille = famille
+        self.couleur = couleur
+        self.numero = numero
+
+class plateau:
+    def __init__(self):
+        self.grille = [[0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0]]
+        self.coups = 0
