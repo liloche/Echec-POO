ecc38cead6a06124a3fac751ffec008c0fbef2b2
liloche <liloche68990@gmail.com>
2024-08-08 22:25:27 +0200

    ajout de méthodes dans la classe Plateau

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-class piont:
+class Piont:
     def __init__(self, famille, couleur, numero):
         self.famille = famille
         self.couleur = couleur
         self.numero = numero
 
-class plateau:
+class Plateau:
     def __init__(self):
         self.grille = [[0, 0, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0],
@@ -15,3 +15,17 @@ class plateau:
                        [0, 0, 0, 0, 0, 0, 0, 0],
                        [0, 0, 0, 0, 0, 0, 0, 0]]
         self.coups = 0
+        self.joueur = 0 # 0 = blancs, 1 = noirs
+        self.blancDehors = []
+        self.noirDehors = []
+    
+    def coups_possibles(self, piont):
+        lst = []
+        return lst
+    
+    def changer_piont(self, piont, coord_x, coord_y):
+        return
+    
+    def tourner_plateau(self):
+        grille_remplacement = [[self.grille[7-i][7-j] for j in range(8)] for i in range(8)]
+        self.grille = grille_remplacement
